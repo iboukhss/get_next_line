c6ee4119c3dbcf23aa7fb9253f6e94249b0ee8c3
iboukhss <151830328+iboukhss@users.noreply.github.com>
2024-04-04 03:32:21 +0200

    added testing structure

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: iboukhss <iboukhss@student.42luxe...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 10:02:32 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/03/31 10:03:14 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/04/02 03:44:47 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stddef.h>
 
 char	*get_next_line(int fd);
 
